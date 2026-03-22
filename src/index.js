89a4ef320973b14a54a2bc5692455cc909b10468
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-22 18:23:05 -0300

    feat(feat_webHeader): Add click event handler to navegator buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "./styles.css";
 
-console.log("It's working");
+const headerNavegator = (() => {
+  function goHome() {
+    throw new Error("Not implemented yet.");
+  }
+
+  function goPizzas() {
+    throw new Error("Not implemented yet.");
+  }
+
+  function goAbout() {
+    throw new Error("Not implemented yet.");
+  }
+
+  function clickEventHandler(e) {
+    const targetID = e.target.id;
+    if (targetID === "goHomeButton") {
+      goHome();
+      return;
+    }
+    if (targetID === "goPizzasButton") {
+      goPizzas();
+      return;
+    }
+    if (targetID === "goAboutButton") {
+      goAbout();
+      return;
+    }
+  }
+
+  const headerNavegator = document.querySelector("#headerNavegator");
+  headerNavegator.addEventListener("click", (e) => clickEventHandler(e));
+})();
