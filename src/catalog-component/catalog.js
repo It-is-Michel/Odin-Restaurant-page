027a995bab4bb9ee2913a64e7b367de2ad5ec2a1
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-24 13:58:46 -0300

    feat(feat_catalogComponent): Add a function to add items

--- src/catalog-component/catalog.js
+++ src/catalog-component/catalog.js
@@ -14,6 +14,36 @@ class Catalog{
 
   #generateDOM() {
     this.#element = document.createElement("div");
+    this.#element.classList.add("catalog");
+  }
+
+  addItem(img, productName, desc, price) {
+    const item = document.createElement("div");
+    item.classList.add("catalog__item");
+    this.#element.appendChild(item);
+
+    const itemImgConteiner = document.createElement("div");
+    itemImgConteiner.classList.add("catalog__itemImgContainer");
+    item.appendChild(itemImgConteiner);
+
+    const itemImg = document.createElement("img");
+    itemImg.setAttribute("src", img);
+    itemImgConteiner.appendChild(itemImg);
+
+    const itemName = document.createElement("h3");
+    itemName.classList.add("catalog__itemName");
+    itemName.textContent = productName;
+    item.appendChild(itemName);
+
+    const itemDesc = document.createElement("p");
+    itemDesc.classList.add("catalog__itemDesc");
+    itemDesc.textContent = desc;
+    item.appendChild(itemDesc);
+
+    const itemPrice = document.createElement("p");
+    itemPrice.classList.add("catalog__itemPrice");
+    itemPrice = `Price: $${price}`;
+    item.appendChild(itemPrice);
   }
 }
 
